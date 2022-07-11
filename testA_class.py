4dd507d64513829a3ad0f984e079d3385b68d29c
vagrant <vagrant@python>
2022-07-11 02:35:24 +0000

    2022-07-10

--- testA_class.py
+++ testA_class.py
@@ -1,13 +1,12 @@
 class classA:
-
     def __init__(self):
         self.xyz = 'Instance Attribute from ClassA'
 
     val = 'Class Attribute from testA'
 
-    def newFunc():
-        return __name__
+def newFunc():
+    return __name__
 
-    if __name__ == '__main__':
-        val = newFunc()
-        print(val)
+if __name__ == '__main__':
+    val = newFunc()
+    print(val)
--- testB_class.py
+++ testB_class.py
@@ -1,13 +1,12 @@
 class classB:
-
     def __init__(self):
         self.xyz = 'Instance Attribute from ClassB'
 
     val = 'Class Attribute from testB'
 
-    def newFunc():
-        return __name__
+def newFunc():
+    return __name__
 
-    if __name__ == '__main__':
-        val = newFunc()
-        print(val)
+if __name__ == '__main__':
+    val = newFunc()
+    print(val)
